0cdd205f838e08473dec4afe9e39322cb0f1cb99
Dave Doolin <david.doolin@gmail.com>
2013-01-02 20:56:03 -0800

    added comparator tests, pending out

--- test/test_comparators.cpp
+++ test/test_comparators.cpp
@@ -30,6 +30,7 @@ public:
     make_records_vector(is, records);
   }
 
+
   void test_name_similarity() {
 
     describe_test(INDENT2, __PRETTY_FUNCTION__);
@@ -42,8 +43,59 @@ public:
   }
 
 
+  void test_location_similarity() {
+
+    describe_test(INDENT2, __PRETTY_FUNCTION__);
+
+    Spec spec;
+    spec.xit("From location similarity test %d", DO_SPEC_HANDLE {
+        sprintf(spec.buf, d, 1);
+        return true;
+    });
+  }
+
+  void test_assignee_similarity() {
+
+    describe_test(INDENT2, __PRETTY_FUNCTION__);
+
+    Spec spec;
+    spec.xit("From assignee similarity test %d", DO_SPEC_HANDLE {
+        sprintf(spec.buf, d, 1);
+        return true;
+    });
+  }
+
+  void test_class_similarity() {
+
+    describe_test(INDENT2, __PRETTY_FUNCTION__);
+
+    Spec spec;
+    spec.xit("From class similarity test %d", DO_SPEC_HANDLE {
+        sprintf(spec.buf, d, 1);
+        return true;
+    });
+  }
+
+
+  void test_coauthor_similarity() {
+
+    describe_test(INDENT2, __PRETTY_FUNCTION__);
+
+    Spec spec;
+    spec.xit("From coauthor similarity test %d", DO_SPEC_HANDLE {
+        sprintf(spec.buf, d, 1);
+        return true;
+    });
+  }
+
+
   void runTest() {
     test_name_similarity();
+    test_location_similarity();
+    test_assignee_similarity();
+    test_class_similarity();
+    test_coauthor_similarity();
+    test_coauthor_similarity();
   }
 
 };
