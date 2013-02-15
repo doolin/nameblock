dda9f4ef8831d3abf5efafd05886906a9c5cecb6
Dave Doolin <david.doolin@gmail.com>
2013-02-15 08:35:08 -0800

    first similarity test added.

--- test/test_similarity.cpp
+++ test/test_similarity.cpp
@@ -13,13 +13,39 @@
 #include "testutils.h"
 
 using std::ifstream;
+using std::string;
+using std::vector;
 
+typedef vector<uint8_t> SP;
+
+SP
+compare_records_1(Record * r1, Record * r2) {
+  return SP({1,1});
+}
 
 class SimilarityTest : public CppUnit::TestCase {
 
 public:
 
+  SimilarityTest(string name) : CppUnit::TestCase(name) {
+
+    describe_test(INDENT0, name.c_str());
+  }
+
+  void test_similarity_equal() {
+
+
+    Spec spec;
+    spec.it("Testing similarity for identical records", DO_SPEC {
+        Record * r1 = new Record("Foo, Bar");
+        Record * r2 = new Record("Foo, Bar");
+        SP sp({1,1});
+        return (sp == compare_records_1(r1, r2));
+    });
+  }
+
   void run_tests() {
+    test_similarity_equal();
   }
 
 };
@@ -27,7 +53,7 @@ public:
 void
 test_similarity() {
 
-  SimilarityTest * st = new SimilarityTest();
+  SimilarityTest * st = new SimilarityTest(string("Similarity testing..."));
   delete st;
   st->run_tests();
 }
