d141911e4ff0b8af2e13a930bd8913646f049b93
Dave Doolin <david.doolin@gmail.com>
2013-02-15 17:14:30 -0800

    location class filling out

--- include/location.h
+++ include/location.h
@@ -1,8 +1,21 @@
 #ifndef PATENT_LOCATION_H
 #define PATENT_LOCATION_H
 
+#include <string>
 
 class Location {
+
+public:
+  std::string * street;
+  std::string * city;
+  std::string * state;
+  std::string * country;
+  std::string * zipcode;
+  // Let's convert these ahead of time and
+  // store them explicitly, because that
+  // will save a lot of run time conversion
+  float latitude;
+  float longitude;
 };
 
 #endif  /* PATENT_LOCATION_H */
